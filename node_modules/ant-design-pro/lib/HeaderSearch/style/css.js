9d5481ec0ecbb122d7bc77cd7e2d43a676957836
ervinanoh <ervin.anoh@savoirfairelinux.com>
2021-05-04 14:38:09 -0400

    clean up du projet, reset after

--- node_modules/ant-design-pro/lib/HeaderSearch/style/css.js
+++
@@ -1,4 +0,0 @@
-require('./index.css');
-require('antd/lib/auto-complete/style/css');
-require('antd/lib/input/style/css');
-require('antd/lib/icon/style/css');
--- node_modules/ant-design-pro/lib/HeaderSearch/style/index.js
+++
@@ -1,4 +0,0 @@
-require('./index.less');
-require('antd/lib/auto-complete/style/index');
-require('antd/lib/input/style/index');
-require('antd/lib/icon/style/index');
