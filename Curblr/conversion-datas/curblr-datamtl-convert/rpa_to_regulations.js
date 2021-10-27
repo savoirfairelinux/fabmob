14473d86dcef90639b7f91d8803b987a582c806a
idillon-sfl <io.daza-dillon@savoirfairelinux.com>
2021-10-27 13:23:00 -0400

    add irrelevant expressions, no parking default with timespan, and months rules to rpa convertion

--- Curblr/conversion-datas/curblr-datamtl-convert/rpa_to_regulations.js
+++ Curblr/conversion-datas/curblr-datamtl-convert/rpa_to_regulations.js
@@ -10,6 +10,52 @@ function debug(...param){
     }
 }
 
+// one or two digits, followed by zero or more spaces, followed by "h" or "min"
+const timeRegex = /\d{1,2}\s*(h|min)/i;
+
+const daysRegexes = {
+    // beginning of a word, followed by the truncated name of a day or its complete name
+    "mo": /\blun(\b|\\|di)/i,
+    "tu": /\bmar(\b|\\|di)/i,
+    "we": /\bmer(\b|\\|credi)/i,
+    "th": /\bjeu(\b|\\|di)/i,
+    "fr": /\bve[nm](\b|\\|dredi)/i, // there is a typo in the data
+    "sa": /\bsam(\b|\\|medi)/i,
+    "su": /\bdim(\b|\\|manche)/i
+};
+
+const monthsRegexes = {
+    // a digit or the beginning of a word, followed by the truncated name of a month or its complete name.
+    "01": /(\d|\b)jan(\b|\.|vier)/i,
+    "02": /(\d|\b)f[eé]v(\b|\.|rier)/i,
+    "03": /(?<!de-)(\d|\b)mar(\b|\.|s)/i, // excludes champ-de-mars
+    "04": /(\d|\b)avr(\b|\.|il)/i,
+    "05": /(\d|\b)mai/i,
+    "06": /(\d|\b)juin/i,
+    "07": /(\d|\b)juillet/i,
+    "08": /(\d|\b)ao[uû]t/i,
+    "09": /(\d|\b)sep(\b|\.|tembre)/i,
+    "10": /(\d|\b)oct(\b|\.|obre)/i,
+    "11": /(\d|\b)nov(\b|\.|embre)/i,
+    "12": /(\d|\b)d[eé]c(\b|\.|embre)/i,
+};
+
+function containsTimespan(description) {
+    return timeRegex.test(description)
+        || Object.values(daysRegexes).some( reg => reg.test(description) )
+        || Object.values(monthsRegexes).some( reg => reg.test(description) );
+}
+
+const irrelevantExpressions = [
+    "RUELLE FERMEE",
+    "PASSAGE INTERDIT",
+    "DECHETS INTERDITS"
+];
+
+function containsIrrelevantExpression(description) {
+    return irrelevantExpressions.some( expression => description.includes(expression));
+}
+
 const fs = require('fs');
 
 const rpaCodificationJson = fs.readFileSync('data/signalisation-codification-rpa.json');
@@ -40,17 +86,27 @@ for (let [key, rpaId] of Object.entries(rpaIds)) {
     rpaInfo.code = rpaCodes[key];
 
     description = rpaDescriptions[key].toUpperCase();
+
+    if (containsIrrelevantExpression(description)) {
+        continue;
+    }
+
     let activity = '';
 
-    if(description.includes("\\P ") || description.startsWith("STAT. INT. ")) {
+    if(description.includes("\\P ") || description.startsWith("/P ") || description.startsWith("STAT. INT. ") || description.startsWith("INTERDICTION DE STAT. ")) {
         activity = 'no parking';
-    } else if(description.includes("\\A ")) {
+    } else if(description.includes("\\A ") || description.startsWith("A ")) {
         activity = 'no standing';
     } else if(description.startsWith("P ")) {
         activity = 'parking';
     } else if(description.startsWith("PANONCEAU ") || description.startsWith("PANNONCEAU")) {
         activity = null;
-    } else {
+    } else if (containsTimespan(description)) {
+        // We assume descriptions containing timespan without further indications are no parking
+        // This might be a wrong assumption
+        activity = 'no parking';
+    }
+    else {
         continue;
         //debug(`${rpaCode.CODE_RPA} ${description}`)
     }
@@ -70,7 +126,10 @@ for (let [key, rpaId] of Object.entries(rpaIds)) {
                     "MARS 01 A DEC. 01":[{"from":"03-01","to":"12-01"}],
                     "1 MARSL AU 1 DEC":[{"from":"03-01","to":"12-01"}],
                     "1MARS AU 1 DEC.":[{"from":"03-01","to":"12-01"}],
+                    "15 MRS - 15 NOV.":[{"from":"03-15","to":"11-15"}],
                     "15 MARS AU 15 NOV":[{"from":"03-15","to":"11-15"}],
+                    "15 MARS À 15 NOV":[{"from":"03-15","to":"11-15"}],
+                    "15 MARS - 15 NOVEMBRE":[{"from":"03-15","to":"11-15"}],
                     "15 MARS AU 15 NOVEMBRE":[{"from":"03-15","to":"11-15"}],
                     "1 AVRIL AU 30 SEPT":[{"from":"04-01","to":"09-30"}],
                     "1 AVRIL AU 15 OCT":[{"from":"05-01","to":"10-15"}],
@@ -95,15 +154,20 @@ for (let [key, rpaId] of Object.entries(rpaIds)) {
                     "15 AVRIL AU 1ER NOV.":[{"from":"04-15","to":"11-01"}],
                     "15 AVRIL AU 1 NOVEMBRE":[{"from":"04-15","to":"11-01"}],
                     "15 AVRIL AU 15 NOVEMBRE":[{"from":"04-15","to":"11-15"}],
+                    "15 AVR - 15 NOV":[{"from":"04-15","to":"11-15"}],
+                    "15 AVR AU 15 NOV":[{"from":"04-15","to":"11-15"}],
                     "15 AVRIL AU 1ER DEC":[{"from":"05-15","to":"12-01"}],
+                    "MAI-JUIN":[{"from":"05-01","to":"06-01"}],
                     "1MAI AU 1 SEPT":[{"from":"05-01","to":"09-01"}],
                     "1MAI AU 1OCT":[{"from":"05-01","to":"10-01"}],
                     "1 MAI AU 1 NOV":[{"from":"06-01","to":"11-01"}],
                     "15 MAI AU 15 OCT":[{"from":"05-15","to":"10-15"}],
                     "15 MAI AU 15 SEPT":[{"from":"05-15","to":"09-15"}],
                     "1 JUIN AU 1 OCT":[{"from":"06-01","to":"10-01"}],
+                    "15 JUIN AU 1ER SEPT.":[{"from":"06-15","to":"09-01"}],
                     "21 JUIN AU 1 SEPT":[{"from":"06-21","to":"09-01"}],
                     "30 JUIN AU 30 AOUT":[{"from":"06-30","to":"08-30"}],
+                    "16 JUIL. AU 4 SEPT.":[{"from":"07-16","to":"09-04"}],
                     "15 AOUT - 28 JUIN":[{"from":"08-15","to":"06-28"}],
                     "20 AOÛT AU 30 JUIN":[{"from":"08-20","to":"06-30"}],
                     "1 SEPT. AU 23 JUIN":[{"from":"09-01","to":"06-23"}],
@@ -113,12 +177,15 @@ for (let [key, rpaId] of Object.entries(rpaIds)) {
                     "SEPT. À JUIN":[{"from":"09-01","to":"06-30"}],
                     "1 SEPT. AU 30 JUIN":[{"from":"09-01","to":"06-30"}],
                     "1 SEPT. AU 31 MAI":[{"from":"09-01","to":"05-31"}],
+                    "1ER SEPT AU 31 MAI":[{"from":"09-01","to":"05-31"}],
                     "1 NOV. AU 31 MARS":[{"from":"11-01","to":"03-31"}],
                     "1 NOV. AU 1 AVRIL":[{"from":"11-01","to":"04-01"}],
                     "1 NOVEMBRE AU 15 AVRIL":[{"from":"11-01","to":"04-15"}],
                     "1 NOV. AU 1 MAI":[{"from":"11-01","to":"05-01"}],
                     "15 NOV. AU 15 MARS":[{"from":"11-15","to":"03-15"}],
                     "15 NOV. AU 1 AVRIL":[{"from":"11-15","to":"04-01"}],
+                    "15 NOV - 15 AVR":[{"from":"11-15","to":"04-15"}],
+                    "15NOV - 15AVRIL":[{"from":"11-15","to":"04-15"}],
                     "16 NOV. AU 14 MARS":[{"from":"11-16","to":"03-14"}],
                     "30 NOV - 1ER AVRIL":[{"from":"11-30","to":"04-01"}],
                     "1 DEC. AU 1 MARS":[{"from":"12-01","to":"03-01"}],
