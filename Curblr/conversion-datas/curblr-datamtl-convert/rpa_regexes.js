d529ca8730b930aa1ccee0cdaefd0514984b989b
idillon-sfl <io.daza-dillon@savoirfairelinux.com>
2021-10-29 17:19:15 -0400

    move regexes in separate file with tests

---
+++ Curblr/conversion-datas/curblr-datamtl-convert/rpa_regexes.js
@@ -0,0 +1,100 @@
+// one or two digits, followed by zero or more spaces, followed by "h", followed by 0 to two digits
+const timeStr = "\\d{1,2}\\s*h\\d{0,2}";
+const time = new RegExp(timeStr, "i");
+
+const timeIntervalStr = `(${timeStr})\\s*[Aaà@-]\\s*(${timeStr})`;
+const timeInterval = new RegExp(timeIntervalStr, "i");
+
+// one or two digits, followed by zero or more spaces, followed by "min"
+const maxStayStr = "\\d{1,2}\\s*min";
+const maxStay = new RegExp(maxStayStr, "i");
+
+// match a sequence of time intervals
+// examples: "6h-7h30" or "6h-7h30 8h À 10h et 11h@12h"
+const timesSequenceStr = `(${timeIntervalStr})(?:\\s+(?:et\\s+)?(${timeIntervalStr}))*`;
+
+// mapping of days of the week with the regex that will match that day
+const daysStrs = {
+    // beginning of a word, followed by the truncated name of a day or its complete name
+    "mo": "\\blun(?:\\b|di)",
+    "tu": "\\bmar(?:\\b|di)",
+    "we": "\\bmer(?:\\b|credi)",
+    "th": "\\bjeu(?:\\b|di)",
+    "fr": "\\bve[nm](?:\\b|dredi)", // there is a typo in the data
+    "sa": "\\bsam(?:\\b|edi)",
+    "su": "\\bdim(?:\\b|anche)"
+};
+const days = Object.entries(daysStrs)
+                          .reduce( (acc, [key, value]) => {
+                              acc[key] = new RegExp(value, "i");
+                              return acc;
+                            }, {});
+
+// regex that will match any day of the week
+const anyDayStr = Object.values(daysStrs).join("|");
+const anyDay = new RegExp(anyDayStr, "i");
+
+// regex that will match any interval of time
+const daysIntervalStr = `(${anyDayStr})\\s+(?:A|À|AU)\\s+(${anyDayStr})`;
+const daysInterval = new RegExp(daysIntervalStr, "i");
+
+// mapping of months names with the regex that will match that month
+const monthsStrs = {
+    // a digit or the beginning of a word, followed by the truncated name of a month or its complete name.
+    "01": "(?:\\d|\\b)jan(?:\\b|vier)",
+    "02": "(?:\\d|\\b)f[eé]v(?:\\b|rier)",
+    "03": "(?<!de-)(?:\\d|\\b)mar(?:\\b|s)", // exclude champ-de-mars
+    "04": "(?:\\d|\\b)avr(?:\\b|il)",
+    "05": "(?:\\d|\\b)mai",
+    "06": "(?:\\d|\\b)juin",
+    "07": "(?:\\d|\\b)jui(?:\\b|llet)",
+    "08": "(?:\\d|\\b)ao[uû]t",
+    "09": "(?:\\d|\\b)sep(?:\\b|tembre)",
+    "10": "(?:\\d|\\b)oct(?:\\b|obre)",
+    "11": "(?:\\d|\\b)nov(?:\\b|embre)",
+    "12": "(?:\\d|\\b)d[eé]c(?:\\b|embre)",
+}
+const months = Object.entries(monthsStrs)
+                          .reduce( (acc, [key, value]) => {
+                              acc[key] = new RegExp(value);
+                              return acc;
+                            }, {});
+
+// regex that will match any day of the week
+const anyMonthStr = Object.values(monthsStrs).join("|");
+const anyMonth = new RegExp(anyMonthStr, "i");
+
+const anyTimespanStr = [
+    timeStr,
+    maxStayStr,
+    anyDayStr,
+    anyMonth,
+].join("|")
+const anyTimespan = new RegExp(anyTimespanStr, "i");
+
+const noTimespanBeforeStr = `(?<!(${anyTimespanStr}).*)`;
+const noTimespanAfterStr = `(?!.*(${anyTimespanStr}))`;
+
+module.exports = {
+    timeStr,
+    time,
+    timeIntervalStr,
+    timeInterval,
+    maxStayStr,
+    maxStay,
+    timesSequenceStr,
+    daysStrs,
+    days,
+    anyDayStr,
+    anyDay,
+    daysIntervalStr,
+    daysInterval,
+    monthsStrs,
+    months,
+    anyMonthStr,
+    anyMonth,
+    anyTimespanStr,
+    anyTimespan,
+    noTimespanBeforeStr,
+    noTimespanAfterStr
+}
---
+++ Curblr/conversion-datas/curblr-datamtl-convert/test/rpa_regexes.test.js
@@ -0,0 +1,86 @@
+const rpaRegex = require("../rpa_regexes");
+
+describe("regexes", () => {
+    test.each([
+        ["1h", true],
+        ["1h5", true],
+        ["1h30", true],
+        ["10h", true],
+        ["10h5", true],
+        ["10h30", true],
+        ["lundi1h", true],
+        ["1hlundi", true],
+        ["10", false],
+        ["1ah", false],
+        ["h30", false],
+    ])("time.test('%s')", (value, expected) => {
+        const result = rpaRegex.time.test(value);
+        expect(result).toBe(expected);
+    });
+
+    test.each([
+        ["1h-2h", true],
+        ["1h - 2h", true],
+        ["1hà2h", true],
+        ["1h à 2h", true],
+        ["1hÀ2h", true],
+        ["1h À 2h", true],
+        ["1ha2h", true],
+        ["1h a 2h", true],
+        ["1hA2h", true],
+        ["1h A 2h", true],
+        ["1h @ 2h", true],
+        ["1h@2h", true],
+        ["1h    -  2h", true],
+        ["1h 2h", false],
+        ["1hb2h", false],
+        ["1h b 2h", false],
+        ["1hàa2h", false],
+        ["lundi à vendredi", false],
+    ])("timeInterval.test('%s')", (value, expected) => {
+        const result = rpaRegex.timeInterval.test(value);
+        expect(result).toBe(expected);
+    });
+
+    test.each([
+        ["LUN", true],
+        ["LUN.", true],
+        ["LUNDI", true],
+        ["MAR", true],
+        ["MAR.", true],
+        ["MARDI", true],
+        ["MER", true],
+        ["MER.", true],
+        ["MERCREDI", true],
+        ["JEU", true],
+        ["JEU.", true],
+        ["JEUDI", true],
+        ["VEN", true],
+        ["VEN.", true],
+        ["VEMDREDI", true],
+        ["VENDREDI", true],
+        ["SAM", true],
+        ["SAM.", true],
+        ["SAMEDI", true],
+        ["DIM", true],
+        ["DIM.", true],
+        ["DIMANCHE", true],
+        ["12h LUN 1 MARS", true],
+        ["MONDAY", false],
+        ["MARS", false],
+    ])("days.test('%s')", (value, expected) => {
+        const result = rpaRegex.anyDay.test(value);
+        expect(result).toBe(expected);
+    });
+
+    test.each([
+        ["LUN À VEN", true],
+        ["LUN A VEN", true],
+        ["LUN AU VEN", true],
+        ["LUN VEN", false],
+        ["1h À 2h", false]
+    ])("daysInterval.test('%s')", (value, expected) => {
+        const result = rpaRegex.daysInterval.test(value);
+        expect(result).toBe(expected);
+    });
+})
